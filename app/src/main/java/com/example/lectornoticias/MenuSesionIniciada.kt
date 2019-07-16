bedbf9454528f260019925b48182959960a441ba
Jose <andrangel97@gmail.com>
2019-07-15 20:26:49 -0600

    Agregado login

---
+++ app/src/main/java/com/example/lectornoticias/MenuSesionIniciada.kt
@@ -0,0 +1,12 @@
+package com.example.lectornoticias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuSesionIniciada : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_sesion_iniciada)
+    }
+}
